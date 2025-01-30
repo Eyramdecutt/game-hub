e6a8f03c5677d9750b862d76de7d5d79ad076368
Eyram Decutt <eyramdecutt@gmail.com>
2025-01-30 15:05:02 +0000

    Displaying genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import useGenres from "../hooks/useGenres";
+import { HStack, Image, List, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
     <ul>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <List key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </List>
       ))}
     </ul>
   );
